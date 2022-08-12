e41764dfdbdcc8eac5808d4f913c2d596d4b9f8b
Code Sandbox <hello@codesandbox.io>
2022-08-12 19:14:11 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.9bddddf35821aaa9e25f48faee192372.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "80074dd0e26981968f97",
+    "url": "https://.github.io/csb-yoh3z6/static/js/main.80074dd0.chunk.js"
+  },
+  {
+    "revision": "15a8f64ae8c57df431b0",
+    "url": "https://.github.io/csb-yoh3z6/static/css/1.434da1d5.chunk.css"
+  },
+  {
+    "revision": "15a8f64ae8c57df431b0",
+    "url": "https://.github.io/csb-yoh3z6/static/js/1.15a8f64a.chunk.js"
+  },
+  {
+    "revision": "99f93fc4f3f995e9283c",
+    "url": "https://.github.io/csb-yoh3z6/static/js/runtime~main.99f93fc4.js"
+  },
+  {
+    "revision": "7e263200d01fb5998c401b03ef3e6433",
+    "url": "https://.github.io/csb-yoh3z6/static/media/summernote.7e263200.eot"
+  },
+  {
+    "revision": "03f45d882ff3a80b719cfc0789c5eab9",
+    "url": "https://.github.io/csb-yoh3z6/static/media/summernote.03f45d88.woff"
+  },
+  {
+    "revision": "549304631a5f1ab69498e294543086e3",
+    "url": "https://.github.io/csb-yoh3z6/static/media/summernote.54930463.ttf"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "https://.github.io/csb-yoh3z6/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "https://.github.io/csb-yoh3z6/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "https://.github.io/csb-yoh3z6/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "https://.github.io/csb-yoh3z6/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "https://.github.io/csb-yoh3z6/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "2641177b0da3e3613d26b64a986f3c01",
+    "url": "https://.github.io/csb-yoh3z6/index.html"
+  }
+];
